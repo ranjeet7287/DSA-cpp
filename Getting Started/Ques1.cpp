e50c280e3653fe4c181a7e90ccbfefe1526d7c46
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-01 15:35:12 +0530

    Output

---
+++ Getting Started/Ques1.cpp
@@ -0,0 +1,11 @@
+/*
+
+#include <iostream>
+using namespace std;
+int main(){
+   cout << code ;
+}
+If we want to print something on screen exactly, we need to put that text in
+double quotes, otherwise error will come. For eg. cout << "code";
+
+*/
