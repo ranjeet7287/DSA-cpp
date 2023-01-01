6fad909c8512865a6f1e0820cf07aa515569c2d8
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-01 16:10:16 +0530

    Create ques4.cpp

---
+++ Getting Started/ques4.cpp
@@ -0,0 +1,12 @@
+// What is the output of the following code if the input is :
+// 2, 10.1, D ?
+#include <iostream>
+using namespace std;
+int main() {
+    int a;
+    double d;
+    char c;
+    cin >> a >> d >> c;
+    cout << a << d << c << endl;
+}
+// ans -> 210.1D
