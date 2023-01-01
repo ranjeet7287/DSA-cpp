9913219fefbc11a579dd3e9843137cc55fb1d390
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-01 15:58:34 +0530

    Create DataType.cpp

---
+++ Getting Started/DataType.cpp
@@ -0,0 +1,14 @@
+/*
+Which of the following data type stores longest decimal number ?
+long
+float
+double
+short
+
+
+Solution Description
+Out of all given options, only float and double can hold decimal numbers. 
+Size of the float is 4 bytes and double is 8 bytes (in most of the compilers, 
+as the size of data types is compiler specific). 
+So double can store bigger decimal numbers.
+*/
