2c382e997bdc9464c2fe9362298a14ec239e1041
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-01 16:03:20 +0530

    Update garbage.cpp

--- Getting Started/garbage.cpp
+++ Getting Started/garbage.cpp
@@ -1,5 +1,8 @@
-/*
-
-In C++, all variables contain garbage values before their initialisation.
-
-*/
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cout<<a+b;
+}
+// In C++, all variables contain garbage values before their initialisation.
