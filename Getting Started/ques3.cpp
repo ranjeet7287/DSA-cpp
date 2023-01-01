e16f0b371a13274dc38330bbc1153489fc08a9f8
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-01 16:09:04 +0530

    Create ques3.cpp

---
+++ Getting Started/ques3.cpp
@@ -0,0 +1,12 @@
+// What is the output of the following code if the input is :
+// 5, 15 ?
+  #include <iostream>
+  using namespace std;
+  int main() {
+    int a, b; 
+    cin >> a;
+    cin >> b;
+    cout << (a+b);
+}
+
+// ans 20
