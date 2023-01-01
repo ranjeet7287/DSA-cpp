1523e9970896700d99036e45af9b273490d4af7d
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-01 18:10:42 +0530

    code

---
+++ Getting Started/Typecast.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=100;
+    // intger to char
+    char c=a;
+    cout<<c<<endl;
+    // char to int
+    c='y';
+    a=c;
+    cout<<a<<endl;
+}
