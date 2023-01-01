81f410e0bf467d87bfe61c4a9c2138ca2a0bbb95
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-01 19:30:49 +0530

    Create ques7.cpp

---
+++ Getting Started/ques7.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int var1 = 5;
+    int var2 = 6;
+    cout << (var1 > var2);
+}
+
+// ans = 0
+// '>' operator gives a boolean answer. And the condition is false. 
+// So answer will be false and 0 will be printed (boolean variables print only 0 and 1).
