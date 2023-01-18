454c202d0307c605dd63626a753dfdc260db36da
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-18 13:16:13 +0530

    Create isoceles.cpp

---
+++ Pattern2/isoceles.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n)
+    {
+        int k=1;
+        while(k<=n-i)
+        {
+            cout<<" ";
+            k++;
+        }
+        int j=1;
+        while(j<=i)
+        {
+            cout<<j;
+            j++;
+        }
+        int y=i-1;
+        while(y>=1)
+        {
+            cout<<y;
+            y--;
+        }
+        cout<<endl;
+        i++;
+    }
+}
