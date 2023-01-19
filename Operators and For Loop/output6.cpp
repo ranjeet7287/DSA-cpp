a41ee05c917398191d2997883b3a251668be7ff3
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-19 19:00:30 +0530

    Create output6.cpp

---
+++ Operators and For Loop/output6.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=1;
+    while(i<5)
+    {
+        if(i==3)
+        {
+            break;
+        }
+        cout<<i<<" ";
+        i++;
+    }
+}
+// 1 2 
