b5ac71d1d977bb1479099b8d13ecf97b8946a923
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-19 18:58:19 +0530

    Create skipiteration.cpp

---
+++ Operators and For Loop/skipiteration.cpp
@@ -0,0 +1,12 @@
+/*
+Which of these jump statements can 
+skip processing remainder of code in its body for a particular iteration ?
+
+ans->
+Solution Description
+####"break" is used to exit from the current loop.
+####"return" statement is used to exit from the current function.
+####"continue" is used to skip the current iteration of a 
+loop and continue with the next iteration.
+
+*/
