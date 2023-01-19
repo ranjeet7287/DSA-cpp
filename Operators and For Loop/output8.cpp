dc9a50cd6d7e8b49b301424004449bb946d7f088
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-19 19:05:10 +0530

    Create output8.cpp

---
+++ Operators and For Loop/output8.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i = 1;
+    while(i < 3) {
+    int j = 1;
+    while(j < 5) {
+        if(j == 3) {
+            break;
+        }
+        cout << j << " ";
+        j++;
+    }
+    i++;
+    }
+}
+// 1 2 1 2
