c58d871682f2456017008d69263e1b18b4e080fa
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-20 12:36:29 +0530

    Create arraydecleration.cpp

---
+++ Array/arraydecleration.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr[10];
+    // Array of DataType Integer and have index from 0 to 9.
+    // This array contain Grabage value at every index. 
+}
