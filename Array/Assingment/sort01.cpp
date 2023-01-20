3bee89d87858440523072b3e4738fc63faa7143a
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-20 16:25:30 +0530

    Create sort01.cpp

---
+++ Array/Assingment/sort01.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void sortZeroesAndOne(int *input, int size)
+{
+    int count = 0; 
+    for (int i = 0; i < size; i++) {
+        if (input[i] == 0)
+            count++;
+    }
+    for (int i = 0; i < count; i++)
+        input[i] = 0;
+    for (int i = count; i < size; i++)
+        input[i] = 1;
+}
+
+int main()
+{
+
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		int size;
+
+		cin >> size;
+		int *input = new int[size];
+
+		for (int i = 0; i < size; ++i)
+		{
+			cin >> input[i];
+		}
+
+		sortZeroesAndOne(input, size);
+
+		for (int i = 0; i < size; ++i)
+		{
+			cout << input[i] << " ";
+		}
+
+		cout << endl;
+		delete[] input;
+	}
+
+	return 0;
+}
