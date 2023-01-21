4eea8401274c8958826feabd42dddce8f45dbdcd
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-21 09:39:54 +0530

    Create linearsearch.cpp

---
+++ searchingandshorting/linearsearch.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int linearSearch(int *arr,int n,int x)
+{
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==x)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int *arr=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int x;
+    cin>>x;
+    int ans=linearSearch(arr,n,x);
+    cout<<ans;
+    delete[]arr;
+}
