ef7853352468e57659093da877ac888bf66ab714
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-21 10:16:53 +0530

    Create Binarysearch.cpp

---
+++ searchingandshorting/Binarysearch.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using  namespace std;
+int BinarySearch(int *arr,int n,int x)
+{
+    int start=0;
+    int end=n-1;
+    while(start<=end)
+    {
+        int mid=start+end/2;
+        if(x==arr[mid])
+        {
+            return mid;
+        }else if(x<arr[mid])
+        {
+            end=mid-1;
+        }else{
+            start=mid+1;
+        }
+    }
+    return -1;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int *arr=new int[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    int x;
+    cin>>x;
+    BinarySearch(arr,n,x);
+}
