3f580f4efa8031c4c0a91bd2cb48fd52aa255263
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-23 11:55:02 +0530

    Create copystring.cpp

---
+++ Char and 2D Array/String/copystring.cpp
@@ -0,0 +1,15 @@
+// strcpy(destination_strig,source_string);
+// a='rajeet';
+// b='singh';
+// strcpy(a,b); (a will copy the value of b) ;
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char arr1[100]="ranjeet";
+    char arr2[100]="singh";
+
+    strcpy(arr1,arr2);
+    cout<<arr1; //singh
+}
