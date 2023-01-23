612a09e5f8e075031fffdfd1a867b0ac7ad354b2
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-23 10:38:49 +0530

    Create Cingetline.cpp

---
+++ Char and 2D Array/Cingetline.cpp
@@ -0,0 +1,15 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     char arr1[100];
+//     cin>>arr1; // Hello World
+//     cout<<"Result of Cin"<<arr1<<endl; // Hello Reason--> Cin avoid Newline,Tab so teminator(\0) come at nth legnth of Array
+
+//     char arr2[100];
+//     cin.getline(arr2,100); // Hello World
+//     cout<<"Result of cin.getline()"<<arr2; // Hello World 
+//     // Reason --> Read  newline,space,tab .
+// }
+
+
