a864728595dc82e82c998e75ce3d172ee2ee0aef
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-24 10:09:59 +0530

    Update address.cpp

--- Pointer/address.cpp
+++ Pointer/address.cpp
@@ -1,4 +1,6 @@
 //Pointer are variables which store address of other variable .
+// ex  ---> datatype i;
+// Syntax--> datatype * name = &i;
 #include<iostream>
 using namespace std;
 int main()
@@ -6,4 +8,10 @@ int main()
     int j=1;
     // address of j  --> &j
     cout<<&j<<endl; //0x61ff0c in Hexadecimal value 
+
+    int i=1;
+    int *p=&i;
+    cout<<p<<endl;  // 0x61ff04 p store the adress of i .
+    cout<<&i<<endl; // 0x61ff04 printing address of i .
+
 }
