34b3e42db3ec9a4e260828d0b41674bde28d9d1d
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-24 15:49:51 +0530

    Create ques9.cpp

---
+++ Pointer/Assignment/ques9.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int ***r, **q, *p, i=8;
+  p = &i;
+  (*p)++;
+  q = &p;
+  (**q)++;
+  r = &q;
+  cout<<*p << " " <<**q << " "<<***r;
+  return 0;
+}
+// 10 10 10
