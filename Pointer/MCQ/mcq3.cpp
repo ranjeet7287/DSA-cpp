54689113ee9982d3a1816d17ddb18e7d69d6982a
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-24 10:47:19 +0530

    Create mcq3.cpp

---
+++ Pointer/MCQ/mcq3.cpp
@@ -0,0 +1,9 @@
+// Which of the following gives the memory address of variable 'b' pointed by pointer 'a' i.e.
+// int b = 10;
+// int *a = &b;
+
+
+//  a  --> right ans
+// *a
+// &a
+// address(a)
