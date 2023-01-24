f7d9fffe97fbde0ca11bfa6d0c3153dfced1dd46
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-24 10:48:40 +0530

    Create mcq4.cpp

---
+++ Pointer/MCQ/mcq4.cpp
@@ -0,0 +1,10 @@
+// What will happen in this code?
+// int a = 100, b = 200;
+// int *p = &a, *q = &b;
+// p = q;
+
+
+// b is assigned to a
+// p now points to b  <-- Right answer
+// a is assigned to b
+// q now points to a
