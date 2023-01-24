00a8133f5d09606eac2ede0dff70b5cb61dcfded
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-24 15:44:02 +0530

    Create ques1.cpp

---
+++ Pointer/Assignment/ques1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int arr[] = {4, 5, 6, 7};
+  int *p = (arr + 1);
+  cout << *arr + 9; //(*arr-->arr[0])  (4+9-->13)
+  return 0;
+}
+// 13
