a6c93455fe4118971a35f0c4312d05bc294c1c9c
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-27 10:36:15 +0530

    Create decleration.cpp

---
+++ Dynamic_Allocation/RefrenceVariable/decleration.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    // WRONG WAY OF DECLERATION OF REFRENCE VARIABLE
+    // int i=1;
+    // int &j;
+    // j=i;
+
+    // RIGHT WAY OF DECLERATION OF REFRENCE VARIABLE
+    int i=1;
+    int &j=i;
+    
+
+}
