b88c9bf2961607db5243307f0ed04cd5a9ff2dff
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-28 16:16:56 +0530

    Create mcq1.cpp

---
+++ Recursion/mcq/mcq1.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+using namespace std;
+
+void print(int n){
+    if(n < 0){
+        return;
+    }
+    cout << n << " ";
+    print(n - 2);
+}
+
+int main() {
+    int num = 5;
+    print(num);
+}
+
+// 5 3 1
