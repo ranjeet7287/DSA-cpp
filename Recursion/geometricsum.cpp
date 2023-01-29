cd0016b2c40f9f06e5f5729b81a9fcb92789360e
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-01-29 16:35:29 +0530

    Create geometricsum.cpp

---
+++ Recursion/geometricsum.cpp
@@ -0,0 +1,50 @@
+// Geometric Sum
+// Send Feedback
+// Given k, find the geometric sum i.e.
+// 1 + 1/2 + 1/4 + 1/8 + ... + 1/(2^k) 
+// using recursion.
+// Input format :
+// Integer k
+// Output format :
+// Geometric sum (upto 5 decimal places)
+// Constraints :
+// 0 <= k <= 1000
+// Sample Input 1 :
+// 3
+// Sample Output 1 :
+// 1.87500
+// Sample Input 2 :
+// 4
+// Sample Output 2 :
+// 1.93750
+// Explanation for Sample Input 1:
+// 1+ 1/(2^1) + 1/(2^2) + 1/(2^3) = 1.87500
+
+#include <iostream>
+#include <math.h>
+#include <iomanip>
+using namespace std;
+
+double geometricSum(int k) 
+{
+    if(k==0){
+        return 1;
+    }
+    double small=geometricSum(k-1);
+    return small+1/(double)pow(2,k);
+}
+
+
+
+
+
+int main() {
+    int k;
+    cin >> k;
+    cout << fixed << setprecision(5);
+    cout << geometricSum(k) << endl;   
+}
+
+
+
+
