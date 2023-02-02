b837788bd40b82619b329f8690685625c21d69c0
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-02-02 14:22:49 +0530

    Create removex.cpp

---
+++ Recursion2/removex.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void Removex(char *arr,char x)
+{
+    if(arr[0]!=x)
+    {
+        
+    }
+}
+
+int main()
+{
+    char arr[100];
+    cin>>arr;
+
+    Removex(arr);
+    cout<<ans<<endl;
+    return 0;
+}
