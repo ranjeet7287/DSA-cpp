1c10c77b1fe5b434e8ce12e888d2978d46f832c0
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-02-17 10:55:30 +0530

    Create Questions.cpp

---
+++ Linked_List_2/Questions.cpp
@@ -0,0 +1,40 @@
+// QUSE 1
+// Circular doubly LL
+// Send Feedback
+// Given an unsorted circular doubly linked list, suppose you have reference (or pointer) to its head node only, which of the following operation can be implemented in O(1) time?
+// i) Insertion at the front of the linked list
+// ii) Insertion at the end of the linked list
+// iii) Deletion of the last node of the linked list
+// iv) Deletion of the front node of the linked list
+
+// Ans -> 1 2 3 4 
+
+
+// QUES 2
+// Traversal in LL
+// In doubly linked lists, traversal can be done in ?
+// Ans 2 -> Both Direction -- > Reverse and Forward
+
+
+
+// QUes3 
+
+// Double LL
+// Given an unsorted doubly Linked List, suppose you have references (or pointer) to its head and tail nodes, which of the following operation can be implemented in O(1) time ?
+// i) Insertion at the front of the linked list
+// ii) Insertion at the end of the linked list
+// iii) Deletion of the last node of the linked list
+// iv) Deletion of the front node of the linked list
+
+// ANs -->I,II, III and IV
+
+
+// ques 4
+// Circular LL
+// Given an unsorted circular linked list, suppose you have reference (or pointer) to its head node only, which of the following operation can be implemented in O(1) time?
+// i) Insertion at the front of the linked list
+// ii) Insertion at the end of the linked list
+// iii) Deletion of the last node of the linked list
+// iv) Deletion of the front node of the linked list
+
+// Ans -->  None 
