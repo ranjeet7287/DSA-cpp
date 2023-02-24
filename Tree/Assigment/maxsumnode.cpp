7815108dd92c16703364c0ffa9beaa1b807d1212
ranjeet7287 <116716540+ranjeet7287@users.noreply.github.com>
2023-02-24 17:20:26 +0530

    Create maxsumnode.cpp

---
+++ Tree/Assigment/maxsumnode.cpp
@@ -0,0 +1,99 @@
+// Node with maximum child sum
+// Send Feedback
+// Given a generic tree, find and return the node for which sum of its
+// data and data of all its child nodes is maximum. In the sum, data of the node
+// and data of its immediate child nodes has to be taken.
+// Input format :
+// The first line of input contains data of the nodes of the tree in level order form. 
+// The order is: data for root node, number of children to root node, data of each of child 
+// nodes and so on and so forth for each node. The data of the nodes of the tree is separated 
+// by space. 
+// Output format :
+// The first and only line of output contains the data of the node with maximum sum,
+// as described in the task.
+// Constraints:
+// Time Limit: 1 sec
+// Sample Input 1 :
+// 5 3 1 2 3 1 15 2 4 5 1 6 0 0 0 0
+// Sample Output 1 :
+// 1
+
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+template <typename T>
+class TreeNode {
+   public:
+    T data;
+    vector<TreeNode<T>*> children;
+
+    TreeNode(T data) { this->data = data; }
+
+    ~TreeNode() {
+        for (int i = 0; i < children.size(); i++) {
+            delete children[i];
+        }
+    }
+};
+
+TreeNode<int> *maxsum(TreeNode<int> *root){
+    TreeNode<int> *max=NULL;
+    int total=0;
+    queue<TreeNode<int>*> pending;
+    pending.push(root);
+    while(pending.size()!=0)
+    {
+        TreeNode<int> *front=pending.front();
+        pending.front();
+        int sum=front->data;
+        for(int i=0;i<root->children.size();i++){
+            sum +=front->children[i]->data;
+            pending.push(root->children[i]);
+        }
+        if(sum>total)
+        {
+            total=sum;
+            max=front;
+        }
+    }
+    return max;
+
+}
+
+TreeNode<int>* takeInputLevelWise() {
+    int rootData;
+    cin >> rootData;
+    TreeNode<int>* root = new TreeNode<int>(rootData);
+
+    queue<TreeNode<int>*> pendingNodes;
+
+    pendingNodes.push(root);
+    while (pendingNodes.size() != 0) {
+        TreeNode<int>* front = pendingNodes.front();
+        pendingNodes.pop();
+        int numChild;
+        cin >> numChild;
+        for (int i = 0; i < numChild; i++) {
+            int childData;
+            cin >> childData;
+            TreeNode<int>* child = new TreeNode<int>(childData);
+            front->children.push_back(child);
+            pendingNodes.push(child);
+        }
+    }
+
+    return root;
+}
+
+int main() {
+    TreeNode<int>* root = takeInputLevelWise();
+
+    TreeNode<int>* ans = maxSumNode(root);
+
+    if (ans != NULL) {
+        cout << ans->data;
+    }
+}
+
