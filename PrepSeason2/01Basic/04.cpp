2a40d3b480753951f73c9afd6ce0ea25c2d4ade6
Ranjeet Singh <sranjeet434@gmail.com>
2024-08-18 22:17:51 +0530

    delteing

--- PrepSeason2/01Basic/04.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    char a = 194;
-    cout<<a<<endl;
-}
--- PrepSeason2/01Basic/05.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int  f;
-    cin >> f;
-    int c = (5.0/9.0) * (f-32);
-    cout<<c;
-}
